3b54764183bea4927f6fa2af364057f565802d0b
reZerOR <gkraihan14k@gmail.com>
2024-11-10 22:59:11 +0600

    book controller, services, routes created

---
+++ prisma/migrations/20241110161127_1st/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "bookId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "genre" TEXT NOT NULL,
+    "publishedYear" INTEGER NOT NULL,
+    "totalCopies" INTEGER NOT NULL,
+    "availableCopies" INTEGER NOT NULL,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("bookId")
+);
+
+-- CreateTable
+CREATE TABLE "members" (
+    "memberId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "membershipDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "members_pkey" PRIMARY KEY ("memberId")
+);
+
+-- CreateTable
+CREATE TABLE "borrow-records" (
+    "borrowId" TEXT NOT NULL,
+    "borrowDate" TIMESTAMP(3) NOT NULL,
+    "returnDate" TIMESTAMP(3),
+    "bookId" TEXT NOT NULL,
+    "memberId" TEXT NOT NULL,
+
+    CONSTRAINT "borrow-records_pkey" PRIMARY KEY ("borrowId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "members_email_key" ON "members"("email");
+
+-- AddForeignKey
+ALTER TABLE "borrow-records" ADD CONSTRAINT "borrow-records_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "books"("bookId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrow-records" ADD CONSTRAINT "borrow-records_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "members"("memberId") ON DELETE RESTRICT ON UPDATE CASCADE;
